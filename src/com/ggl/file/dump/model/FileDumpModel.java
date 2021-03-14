2f1e07f47f245a7b28b8426ff76af6d96053cebb
Gilbert G. Le Blanc <ggleblanc2@gmail.com>
2021-03-14 11:02:21 -0500

    Add files via upload

---
+++ src/com/ggl/file/dump/model/FileDumpModel.java
@@ -0,0 +1,17 @@
+package com.ggl.file.dump.model;
+
+import java.io.File;
+
+public class FileDumpModel {
+	
+	private File inputFile;
+
+	public File getInputFile() {
+		return inputFile;
+	}
+
+	public void setInputFile(File inputFile) {
+		this.inputFile = inputFile;
+	}
+
+}
